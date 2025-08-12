440932d97307e7b29ae81c7fee76d2a21fd872b3
CN-SuperStar <1322644172@qq.com>
2025-08-12 21:15:02 +0800

    feat: 用户端登录页构建

---
+++ client/src/views/Login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="login-body">
+    <div class="login-panel">
+      <div class="login-title">用户登录</div>
+      <el-form ref="formRef" :model="formData" :rules="rules" label-width="0px">
+        <el-form-item prop="account">
+          <el-input v-model="formData.account" placeholder="请输入账号" size="large">
+            <template #prepend>
+              <span class="iconfont icon-account"></span>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="formData.password" placeholder="请输入密码" size="large">
+            <template #prepend>
+              <span class="iconfont icon-password"></span>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="checkCode">
+          <div class="check-code-panel">
+            <el-input v-model="formData.checkCode" placeholder="请输入验证码" size="large">
+              <template #prepend>
+                <span class="iconfont icon-checkcode"></span>
+              </template>
+            </el-input>
+            <img class="check-code" :src="checkCodeUrl" alt="验证码" />
+          </div>
+        </el-form-item>
+        <el-form-item label="">
+          <el-checkbox v-model="formData.rememberMe" @click="refreshCheckCode">记住我</el-checkbox>
+        </el-form-item>
+        <el-form-item label="">
+          <el-button type="primary" size="large" :style="{ width: '100%' }" @click="handleLogin">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const formRef = ref(null);
+const formData = ref({
+  account: "",
+  password: "",
+  checkCode: "",
+  rememberMe: false
+});
+const rules = {
+  account: [{ required: true, message: "请输入账号", trigger: "blur" }],
+  password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+  checkCode: [{ required: true, message: "请输入验证码", trigger: "blur" }]
+};
+const checkCodeUrl = ref("api/checkCode?" + new Date().getTime());
+
+function refreshCheckCode() {
+  checkCodeUrl.value = "api/checkCode?" + new Date().getTime();
+}
+
+function handleLogin() {
+  formRef.value.validate(valid => {
+    if (valid) {
+      // Perform login logic here
+      console.log("Login successful", formData.value);
+    } else {
+      console.log("Validation failed");
+      return false;
+    }
+  });
+}
+</script>
+
+<style lang="scss">
+.login-body {
+  width: 100%;
+  height: calc(100vh);
+  background-size: cover;
+  background-position: center;
+  background-image: url("@/assets/images/login-bg.jpg");
+
+  .login-panel {
+    float: right;
+    width: 350px;
+    margin-top: 150px;
+    margin-right: 100px;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 2px 2px 10px #ddd;
+    background: #fff;
+    .login-title {
+      font-size: 20px;
+      text-align: center;
+      margin-bottom: 10px;
+    }
+    .check-code-panel {
+      display: flex;
+      .check-code {
+      }
+    }
+  }
+}
+</style>
